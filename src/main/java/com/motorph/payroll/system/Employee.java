82d51c3c054a2c799bbcb4a4879c75a08c62f8d3
djkier <djjustinekier@gmail.com>
2025-03-20 17:24:34 +0800

    Add Employee Class, and test a sample line from the CSV files

---
+++ src/main/java/com/motorph/payroll/system/Employee.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.motorph.payroll.system;
+
+/**
+ *
+ * @author djjus
+ */
+public class Employee {
+    private String id;
+    private String lastName;
+    private String firstName;
+    private String birthDate;
+    private String address;
+    private String phone;
+    private String sss;
+    private String philhealth;
+    private String tin;
+    private String pagibig;
+    private String status;
+    private String position;
+    private String immSup;
+    private Double basicSalary;
+    private Double riceSubs;
+    private Double phoneAll;
+    private Double clothingAll;
+    private Double semiMonthlySal;
+    private Double hourlyRate;
+    
+    
+    public Employee (String id, String lastName, String firstName, String birthDate, String address, String phone, String sss, 
+            String philhealth, String tin, String pagibig, String status, String position, String immSup, String basicSalary, 
+            String riceSubs, String phoneAll, String clothingAll, String semiMonthlySal, String hourlyRate) {
+        
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.birthDate = birthDate;
+        this.address = address;
+        this.phone = phone;
+        this.sss = sss;
+        this.philhealth = philhealth;
+        this.tin = tin;
+        this.pagibig = pagibig;
+        this.status = status;
+        this.position = position;
+        this.immSup = immSup;
+        this.basicSalary = numberParser(basicSalary);
+        this.riceSubs = numberParser(riceSubs);
+        this.phoneAll = numberParser(phoneAll);
+        this.clothingAll = numberParser(clothingAll);
+        this.semiMonthlySal = numberParser(semiMonthlySal);
+        this.hourlyRate = numberParser(hourlyRate);
+    }
+    
+    private Double numberParser(String money) {
+        String noComma = money.replace(",", "");
+        String noQuotations = noComma.replace("\"", "");
+        return Double.valueOf(noQuotations);
+        
+    }
+    
+    
+    
+    public String toString(){
+        return """
+               **********************************************
+               Employee no.: """ + this.id +
+                "\nName: " + this.lastName + ", " + this.firstName + 
+                "\nBirthday: " + this.birthDate +
+                "\n**********************************************"
+                ;
+    }
+}
--- src/main/java/com/motorph/payroll/system/PayrollSystem.java
+++ src/main/java/com/motorph/payroll/system/PayrollSystem.java
@@ -4,31 +4,53 @@
 
 package com.motorph.payroll.system;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 /**
  *
  * @author djjus
  */
+
+
 public class PayrollSystem {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Employee> employeeDetailsList = new ArrayList<>();
         
+        
+        //sample record
+        String employee1 = "10001,Garcia,Manuel III,10/11/1983,\"Valero Carpark Building Valero Street 1227, Makati City\",966-860-270,44-4506057-3,820126853951,442-605-657-000,691295330870,Regular,Chief Executive Officer,N/A,\"90,000\",\"1,500\",\"2,000\",\"1,000\",\"45,000\",535.71";
+        
+
         System.out.println("Hello World!");
         System.out.println("Im Here!!");
+        System.out.println(employee1);
         
-        
-        while (true) {
-            System.out.println("Enter a number:");
-            int scan = scanner.nextInt();
+        String[] emp = employee1.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
 
-            if (scan == 1) {
-                break;
-            }
-            
-            System.out.println("Click 1 to exit");
-        }
         
-        scanner.close();
+        Employee employee = new Employee(
+            emp[0], emp[1], emp[2], emp[3], emp[4],
+            emp[5], emp[6], emp[7], emp[8], emp[9],
+            emp[10], emp[11], emp[12], emp[13], emp[14],
+            emp[15], emp[16], emp[17], emp[18]                
+        );
+        
+        System.out.println(employee);
+        
+        
+//        while (true) {
+//            System.out.println("Enter a number:");
+//            int scan = scanner.nextInt();
+//
+//            if (scan == 1) {
+//                break;
+//            }
+//            
+//            System.out.println("Click 1 to exit");
+//        }
+//        
+//        scanner.close();
     }
 }
