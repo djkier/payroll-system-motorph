c5c741a952fb83597f5e921e2d19f899d670b5cc
djkier <djjustinekier@gmail.com>
2025-03-20 15:35:21 +0800

    Initial commit. Add initial Java app files

---
+++ src/main/java/com/motorph/payroll/system/PayrollSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motorph.payroll.system;
+
+/**
+ *
+ * @author djjus
+ */
+public class PayrollSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
