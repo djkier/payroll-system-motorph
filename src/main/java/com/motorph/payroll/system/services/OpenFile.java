aabbfc7e48b87a59032db1bfa715e65dc30504d0
djkier <djjustinekier@gmail.com>
2025-03-31 22:03:12 +0800

    Edit OpenFile Class

--- src/main/java/com/motorph/payroll/system/services/OpenFile.java
+++ src/main/java/com/motorph/payroll/system/services/OpenFile.java
@@ -5,6 +5,8 @@
 package com.motorph.payroll.system.services;
 
 
+import static com.motorph.payroll.system.PayrollSystem.employeeDetailsList;
+import com.motorph.payroll.system.models.Employee;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,18 +17,20 @@ import java.util.ArrayList;
  * @author djjus
  */
 public class OpenFile {
-    private ArrayList<String> list;
+    private final ArrayList<Employee> employeeList;
+
     
     public OpenFile() {
-        this.list = new ArrayList<>();
+        this.employeeList = new ArrayList<>();
     }
     
-    public ArrayList<String> open(String filePath) {
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filePath);
+    public ArrayList<String> open(String fileName) {
+        ArrayList<String> list = new ArrayList<>();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName);
     
         if (inputStream == null) {
             System.out.println("File not found!");
-            return null;
+            return list;
         }
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
@@ -41,6 +45,30 @@ public class OpenFile {
         }
         
         return list;
+    }
+    
+    public ArrayList<Employee> openDetails(String fileName) {
+        ArrayList<String> list = open(fileName);
+        
+        for (int i = 1; i < list.size(); i++) {
+            
+            String[] emp = cleanSplit(list.get(i));
+
+            Employee employee = new Employee(
+                emp[0], emp[1], emp[2], emp[3], emp[4],
+                emp[5], emp[6], emp[7], emp[8], emp[9],
+                emp[10], emp[11], emp[12], emp[13], emp[14],
+                emp[15], emp[16], emp[17], emp[18]                
+            );
+
+            employeeList.add(employee);
+        }
+
+        return employeeList;
+    }
+    
+    public String[] cleanSplit(String line) {
+        return line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
     }
     
 }
